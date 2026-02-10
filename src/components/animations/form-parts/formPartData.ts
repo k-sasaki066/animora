ac768efc966588c266d89ebcccff33ed4a742fda
k-sasaki <k.sasaki.bluesky@gmail.com>
2026-02-10 17:40:45 +0900

    form-partsへ引っ越し

--- src/components/animations/form-parts/formPartData.ts
+++ src/components/animations/form-parts/formPartData.ts
@@ -25,4 +25,80 @@ export const formPartData: FormPartItem[] = [
         title: "Multi Select",
         previewText: "Multi Select Example",
     },
+
+    // radios
+    {
+        key: "radioSlide",
+        title: "Radio Slide",
+        previewText: "Radio Slide Example",
+    },
+    {
+        key: "popRadio",
+        title: "Pop Radio",
+        previewText: "Pop Radio Example",
+    },
+    {
+        key: "colorChangeRadio",
+        title: "Color Change Radio",
+        previewText: "Color Change Radio Example",
+    },
+    {
+        key: "rippleRadio",
+        title: "Ripple Radio",
+        previewText: "Ripple Radio Example",
+    },
+    {
+        key: "segmentedControl",
+        title: "Segmented Control",
+        previewText: "Segmented Control Example",
+    },
+    {
+        key: "iconRadio",
+        title: "Icon Radio",
+        previewText: "Icon Radio Example",
+    },
+
+    // checkboxes
+    {
+        key: "foldCheck",
+        title: "Fold Check",
+        previewText: "Fold Check Example",
+    },
+    {
+        key: "simpleCheck",
+        title: "Simple Check",
+        previewText: "Simple Check Example",
+    },
+    {
+        key: "colorfulCheck",
+        title: "Colorful Check",
+        previewText: "Colorful Check Example",
+    },
+    {
+        key: "cardCheck",
+        title: "Card Check",
+        previewText: "Card Check Example",
+    },
+    {
+        key: "pencilCheck",
+        title: "Pencil Check",
+        previewText: "Pencil Check Example",
+    },
+
+    //rate
+    {
+        key: "starGlow",
+        title: "Star Glow",
+        previewText: "Star Glow Example",
+    },
+    {
+        key: "starInput",
+        title: "Star Input",
+        previewText: "Star Input Example",
+    },
+    {
+        key: "starSlider",
+        title: "Star Slider",
+        previewText: "Star Slider Example",
+    },
 ];
--- src/components/animations/form-parts/formPartMap.ts
+++ src/components/animations/form-parts/formPartMap.ts
@@ -4,9 +4,43 @@ import LiftedLabelInput from "./examples/input/lifted-label/LiftedLabelInput";
 import SoftDropSelect from "./examples/select/soft-drop/SoftDropSelect";
 import MultiSelect from "./examples/select/multi/MultiSelect";
 
+import RadioSlideButton from "./examples/radios/RadioSlideButton";
+import PopRadioButton from "./examples/radios/PopRadioButton";
+import ColorChangeRadio from "./examples/radios/ColorChangeRadio";
+import RippleRadio from "./examples/radios/RippleRadio";
+import SegmentedControlRadio from "./examples/radios/SegmentedControlRadio";
+import IconRadio from "./examples/radios/IconRadio";
+
+import FoldCheck from "./examples/checkboxes/FoldCheck";
+import SimpleCheck from "./examples/checkboxes/SimpleCheck";
+import ColorfulCheck from "./examples/checkboxes/ColorfulCheck";
+import CardCheck from "./examples/checkboxes/CardCheck";
+import PencilCheck from "./examples/checkboxes/PencilCheck";
+
+import StarGlowRate from "./examples/rates/StarGlowRate";
+import StarInputRate from "./examples/rates/StarInputRate";
+import StarSliderRate from "./examples/rates/StarSliderRate";
+
 export const formPartMap: Record<string, ComponentType> = {
     floatingLabel: FloatingLabelInput,
     liftedLabel: LiftedLabelInput,
     softDropSelect: SoftDropSelect,
     multiSelect: MultiSelect,
+
+    radioSlide: RadioSlideButton,
+    popRadio: PopRadioButton,
+    colorChangeRadio: ColorChangeRadio,
+    rippleRadio: RippleRadio,
+    segmentedControl: SegmentedControlRadio,
+    iconRadio: IconRadio,
+
+    foldCheck: FoldCheck,
+    simpleCheck: SimpleCheck,
+    colorfulCheck: ColorfulCheck,
+    cardCheck: CardCheck,
+    pencilCheck: PencilCheck,
+
+    starGlow: StarGlowRate,
+    starInput: StarInputRate,
+    starSlider: StarSliderRate,
 };
