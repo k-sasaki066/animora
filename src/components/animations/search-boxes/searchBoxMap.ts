4d2f354ba646e221034937ac73356eb466ca4263
k-sasaki <k.sasaki.bluesky@gmail.com>
2026-02-15 15:49:38 +0900

    ArcMotionSearchアニメーション追加

--- src/components/animations/search-boxes/searchBoxMap.ts
+++ src/components/animations/search-boxes/searchBoxMap.ts
@@ -1,8 +1,10 @@
 import type { ComponentType } from "react";
 import ElasticFocusSearch from "./examples/ElasticFocusSearch";
 import ExpandSearch from "./examples/ExpandSearch";
+import ArcMotionSearch from "./examples/arc-motion/ArcMotionSearch";
 
 export const searchBoxMap: Record<string, ComponentType> = {
     elasticFocusSearch: ElasticFocusSearch,
     expandSearch: ExpandSearch,
+    arcMotionSearch: ArcMotionSearch,
 };
